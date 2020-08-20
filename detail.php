b29b3388d8c07a5266f3c3f3c095c48b0b9070f1
imacoc <isidro@gruvit.com>
2020-08-20 18:51:01 -0300

    Details

--- detail.php
+++ detail.php
@@ -1,6 +1,23 @@
 <!DOCTYPE html>
 <html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    
+    <?php
+        // SDK de Mercado Pago
+        require __DIR__ .  '/vendor/autoload.php';
+
+        // Agrega credenciales
+        MercadoPago\SDK::setAccessToken('PROD_ACCESS_TOKEN');
+
+        // Crea un objeto de preferencia
+        $preference = new MercadoPago\Preference();
+
+        // Crea un ítem en la preferencia
+        $item = new MercadoPago\Item();
+        $item->title = $_POST['title'];
+        $item->quantity = $_POST['unit'];
+        $item->unit_price = $_POST['price'];
+        $preference->items = array($item);
+        $preference->save();
+    ?>
     <meta name="viewport" content="width=1024">
     <title>Tienda e-commerce</title>
 
@@ -124,13 +141,19 @@
                                             </h3>
                                         </div>
                                         <h3 >
-                                            <?php echo $_POST['price'] ?>
+                                            <?php echo "$" .$_POST['price']; ?>
                                         </h3>
                                         <h3 >
-                                            <?php echo "$" . $_POST['unit'] ?>
+                                            <?php echo  $_POST['unit']; ?>
                                         </h3>
                                     </div>
+                                    <form action="/procesar-pago" method="POST">
+                                    <script
+                                    src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+                                    data-preference-id="<?php echo $preference->id; ?>">
+                                    </script>
                                     <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
